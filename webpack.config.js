96089ad8016c3cb8afd45986d0bdaba0f1842a22
Chris Cannon <cannonc@gmail.com>
2016-10-04 09:36:56 -0700

    Moving some dirs

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/// <binding ProjectOpened='Hot' />
 "use strict";
 var webpack = require('webpack');
 var path = require('path');
@@ -5,72 +6,71 @@ var loaders = require('./webpack.loaders');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const HOST = process.env.HOST || "127.0.0.1";
-const PORT = process.env.PORT || "8888";
+const PORT = process.env.PORT || "3001";
 
 // global css
 loaders.push({
-	test: /[\/\\](node_modules|global)[\/\\].*\.css$/,
-	loaders: [
+    test: /[\/\\](node_modules|global)[\/\\].*\.css$/,
+    loaders: [
 		'style?sourceMap',
 		'css'
-	]
+    ]
 });
 // local scss modules
 loaders.push({
-	test: /[\/\\]src[\/\\].*\.scss/,
-	exclude: /(node_modules|bower_components|public)/,
-	loaders: [
+    test: /[\/\\]src[\/\\].*\.scss/,
+    loaders: [
 		'style?sourceMap',
 		'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
 		'sass'
-	]
+    ]
 });
 
 // local css modules
 loaders.push({
-	test: /[\/\\]src[\/\\].*\.css/,
-	exclude: /(node_modules|bower_components|public)/,
-	loaders: [
+    test: /[\/\\]src[\/\\].*\.css/,
+    loaders: [
 		'style?sourceMap',
 		'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
-	]
+    ]
 });
 
 module.exports = {
-	entry: [
+    entry: [
 		`webpack-dev-server/client?http://${HOST}:${PORT}`,
 		`webpack/hot/only-dev-server`,
-		`./src/index.jsx` // Your appʼs entry point
-	],
-	devtool: process.env.WEBPACK_DEVTOOL || 'cheap-module-source-map',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	resolve: {
-		extensions: ['', '.js', '.jsx']
-	},
-	module: {
-		loaders
-	},
-	devServer: {
-		contentBase: "./public",
-		// do not print bundle build stats
-		noInfo: true,
-		// enable HMR
-		hot: true,
-		// embed the webpack-dev-server runtime into the bundle
-		inline: true,
-		// serve index.html in place of 404 responses to allow HTML5 history
-		historyApiFallback: true,
-		port: PORT,
-		host: HOST
-	},
-	plugins: [
+		`./app/index.jsx` // Your appʼs entry point
+    ],
+    devtool: process.env.WEBPACK_DEVTOOL || 'cheap-module-source-map',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders
+    },
+    devServer: {
+        contentBase: "./public",
+        // do not print bundle build stats
+        noInfo: true,
+        // enable HMR
+        hot: true,
+        // embed the webpack-dev-server runtime into the bundle
+        inline: true,
+        // serve index.html in place of 404 responses to allow HTML5 history
+        historyApiFallback: true,
+        port: PORT,
+        host: HOST
+    },
+    plugins: [
 		new webpack.NoErrorsPlugin(),
 		new webpack.HotModuleReplacementPlugin(),
 		new HtmlWebpackPlugin({
-			template: './src/template.html'
+            title: 'EWS ID Server',
+		    template: './app/template.html'
 		}),
-	]
+    ]
 };
